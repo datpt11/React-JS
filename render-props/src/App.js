f46071467a29b65442c52fc5566263df40259bc7
Dat Phung <tiendat.phung11@gmail.com>
2020-04-04 12:41:27 +0700

    render props p2

--- render-props/src/App.js
+++ render-props/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from './components/List';
-const data = ["a", "b", c]
+import Counter from './components/Counter';
+const data = ["a", "b", "c"];
 function App() {
   return (
     <div className="App">
-      <List data={data} render={(item) => <div>{item}</div>}/>
-      <List data={data} render={(item) => <div>- {item}</div>}/>
-
+      <List data={data} render={(item) => <div>{item}</div>} />
+      <List data={data} render={(item) => <div>- {item}</div>} />
+      <Counter>
+        {({ count }) =>
+          <h1>{count}</h1>
+        }
+      </Counter>
     </div>
   );
 }
