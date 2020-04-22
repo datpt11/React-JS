8d7f4d18c0ab92e28c5d098ca67ef2e7650abd78
Dat Phung <tiendat.phung11@gmail.com>
2020-04-23 00:05:09 +0700

    render props update

--- render-props/src/App.js
+++ render-props/src/App.js
@@ -1,14 +1,17 @@
+// render props la truyen vao props 1 function tra ve component va goi lai function, component sẽ render giá trị trả về của hàm đó thay vì render ra Component như thông thường
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from './components/List';
 import Counter from './components/Counter';
+import Plus from './components/Plus';
+import Minus from './components/Minus';
 const data = ["a", "b", "c"];
 function App() {
   return (
     <div className="App">
-      <List data={data} render={(item) => <div>{item}</div>} />
-      <List data={data} render={(item) => <div>- {item}</div>} />
+      <List data={data} render={(item) => <Plus item={item}/>} />
+      <List data={data} render={(item) => <Minus item={item}/>} />
       <Counter>
         {({ count }) =>
           <h1>{count}</h1>
---
+++ render-props/src/components/Minus.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default function({ item }) {
+    return <div>
+       <h1>- {item}</h1>
+    </div>
+}
