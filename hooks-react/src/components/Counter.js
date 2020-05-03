374d600a6db8fba1ec5890a6cab0d3be956913bf
Dat Phung <tiendat.phung11@gmail.com>
2020-05-03 12:28:31 +0700

    hooks useState

---
+++ hooks-react/src/components/Counter.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+export default function() {
+    const [count, setCount] = useState(0);
+    return <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>increment</button>
+    </div>
+}
