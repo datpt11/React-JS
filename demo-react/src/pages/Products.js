0a7b12b11dcd2e85006e895ce23fad98ffb7ac3b
Dat Phung <tiendat.phung11@gmail.com>
2020-05-13 01:06:41 +0700

    format code

--- demo-react/src/pages/Products.js
+++ demo-react/src/pages/Products.js
@@ -1,78 +1,78 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
-import {
-    Container, Row, Col, Card
-} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import './Products.css';
 import CardItem from './CardItem';
 class Products extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            products: [],
-            currentPage: 1,
-            perPage: 3
-        };
-        this.onChangePage = this.onChangePage.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      currentPage: 1,
+      perPage: 3,
+    };
+    this.onChangePage = this.onChangePage.bind(this);
+  }
+  onChangePage(event) {
+    this.setState({
+      currentPage: Number(event.target.id),
+    });
+  }
+  componentDidMount() {
+    axios.get('http://localhost:8080/products').then((res) => {
+      this.setState({
+        products: res.data,
+      });
+    });
+  }
+  render() {
+    const { products, currentPage, perPage } = this.state;
+    const end = currentPage * perPage;
+    const start = end - perPage;
+    const currentProducts = products.slice(start, end);
+    const renderProducts = currentProducts.map((product, i) => {
+      return (
+        <Col sm='4'>
+          <CardItem key={i} product={product} />
+        </Col>
+      );
+    });
+    const pageNumbers = [];
+    for (let i = 1; i < Math.ceil(products.length / perPage); i++) {
+      pageNumbers.push(i);
     }
-    onChangePage(event) {
-        this.setState({
-            currentPage: Number(event.target.id)
-        });
-    }
-    componentDidMount() {
-        axios.get('http://localhost:8080/products').then(res => {
-            this.setState({
-                products: res.data
-            });
-        });
-    }
-    render() {
-        const { products, currentPage, perPage } = this.state;
-        const end = currentPage * perPage;
-        const start = end - perPage;
-        const currentProducts = products.slice(start, end);
-        const renderProducts = currentProducts.map((product, i) => {
-            return <Col sm="4">
-                <CardItem key={i} product={product} />
-            </Col>
-        });
-        const pageNumbers = [];
-        for (let i = 1; i < Math.ceil(products.length / perPage); i++) {
-            pageNumbers.push(i);
-        }
-        const renderPageNumbers = pageNumbers.map(number => {
-            return <PaginationItem>
-                <PaginationLink id={number} key={number} onClick={this.onChangePage}>
-                    {number}
-                </PaginationLink>
+    const renderPageNumbers = pageNumbers.map((number) => {
+      return (
+        <PaginationItem>
+          <PaginationLink id={number} key={number} onClick={this.onChangePage}>
+            {number}
+          </PaginationLink>
+        </PaginationItem>
+      );
+    });
+    return (
+      <Container>
+        <Row>{renderProducts}</Row>
+        <Row style={{ marginTop: '20px' }}>
+          <Pagination aria-label='Page navigation example'>
+            <PaginationItem>
+              <PaginationLink first href='#' />
             </PaginationItem>
-        });
-        return (
-            <Container>
-                <Row>
-                    {renderProducts}
-                </Row>
-                <Row style={{ marginTop:"20px" }}>
-                    <Pagination aria-label="Page navigation example">
-                        <PaginationItem>
-                            <PaginationLink first href="#" />
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationLink previous href="#" />
-                        </PaginationItem>
-                        {renderPageNumbers}
-                        <PaginationItem>
-                            <PaginationLink next href="#" />
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationLink last href="#" />
-                        </PaginationItem>
-                    </Pagination>
-                </Row>
-            </Container>
-        );
-    }
+            <PaginationItem>
+              <PaginationLink previous href='#' />
+            </PaginationItem>
+            {renderPageNumbers}
+            <PaginationItem>
+              <PaginationLink next href='#' />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink last href='#' />
+            </PaginationItem>
+          </Pagination>
+        </Row>
+      </Container>
+    );
+  }
 }
 export default Products;
--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -3,44 +3,46 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import './TodoItem.css';
 import checkImg from '../img/check.svg';
-import checkCompleteImg from '../img/check-complete.svg'
+import checkCompleteImg from '../img/check-complete.svg';
 class TodoItem extends Component {
-    // constructor(props){
-    //     super(props);
-    //     this.onItemClick = this.onItemClick.bind(this);
+  // constructor(props){
+  //     super(props);
+  //     this.onItemClick = this.onItemClick.bind(this);
+  // }
+
+  render() {
+    // const { item } = this.props; //destructuring, this.props la object
+    // let className = 'TodoItem';
+    // if (item.isComplete) {
+    //     className += ' TodoItem-complete'
     // }
-    
-    render() {
-        // const { item } = this.props; //destructuring, this.props la object
-        // let className = 'TodoItem';
-        // if (item.isComplete) {
-        //     className += ' TodoItem-complete'
-        // }
-        // return (
-        //     <div className={className}>
-        //         <p>{this.props.item.title}</p>
-        //     </div>
-        // ); 
-        const { item, onClick } = this.props; 
-        let url = checkImg;
-        if(item.isComplete) {
-            url = checkCompleteImg;
-        }
-        return (
-            <div className={classNames('TodoItem',{ 
-                'TodoItem-complete': item.isComplete === true 
-            })}>
-                <img onClick={onClick} src={url} alt="" width={32} height={32}/>
-                <p>{this.props.item.title}</p>
-            </div>
-        );
+    // return (
+    //     <div className={className}>
+    //         <p>{this.props.item.title}</p>
+    //     </div>
+    // );
+    const { item, onClick } = this.props;
+    let url = checkImg;
+    if (item.isComplete) {
+      url = checkCompleteImg;
     }
+    return (
+      <div
+        className={classNames('TodoItem', {
+          'TodoItem-complete': item.isComplete === true,
+        })}
+      >
+        <img onClick={onClick} src={url} alt='' width={32} height={32} />
+        <p>{this.props.item.title}</p>
+      </div>
+    );
+  }
 }
 TodoItem.propTypes = {
-    item: PropTypes.shape({
-        isComplete: PropTypes.bool,
-            title: PropTypes.string.isRequired
-    }),
-    onClick: PropTypes.func
+  item: PropTypes.shape({
+    isComplete: PropTypes.bool,
+    title: PropTypes.string.isRequired,
+  }),
+  onClick: PropTypes.func,
 };
 export default TodoItem;
